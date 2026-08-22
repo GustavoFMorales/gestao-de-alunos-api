a81ddee61a88bc5c5d4eb9bb012340348066daff
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2026-08-22 10:07:14 -0300

    Adiciona teste de status 400 e 401 na api de login

--- test/login.test.js
+++ test/login.test.js
@@ -3,17 +3,64 @@ import app from "../src/app.js";
 import { expect } from "chai";
 import postLogin from "../fixtures/postLogin.json" with { type: "json" };
 
-
 describe("Login", () => {
   it("login deve retornar 200 quando o usuario e senha forem corretos", async () => {
-    const bodyLogin = structuredClone(postLogin)
+    const bodyLogin = structuredClone(postLogin);
     const response = await request(app)
       .post("/api/auth/login")
       .set("Content-Type", "application/json")
       .send(bodyLogin);
     expect(response.status).to.equal(200);
-    expect(response.body.token).to.be.a('string');
-    expect(response.body.usuario.id).to.equal('admin-principal');
-    expect(response.body.usuario.nome).to.equal('Administrador do Sistema');
+    expect(response.body.token).to.be.a("string");
+    expect(response.body.usuario.id).to.equal("admin-principal");
+    expect(response.body.usuario.nome).to.equal("Administrador do Sistema");
+  });
+  it("login deve retornar 400 quando o e-mail não for informado", async () => {
+    const bodyLogin = structuredClone(postLogin);
+    bodyLogin.email = "";
+    const response = await request(app)
+      .post("/api/auth/login")
+      .set("Content-Type", "application/json")
+      .send(bodyLogin);
+
+    expect(response.status).to.equal(400);
+    expect(response.body.error).to.equal(
+      'Os campos "email" e "senha" são obrigatórios.',
+    );
+  });
+  it("login deve retornar 400 quando a senha não for informada", async () => {
+    const bodyLogin = structuredClone(postLogin);
+    bodyLogin.senha = "";
+    const response = await request(app)
+      .post("/api/auth/login")
+      .set("Content-Type", "application/json")
+      .send(bodyLogin);
+
+    expect(response.status).to.equal(400);
+    expect(response.body.error).to.equal(
+      'Os campos "email" e "senha" são obrigatórios.',
+    );
+  });
+  it("login deve retornar 401 quando a senha informada for incorreta", async () => {
+    const bodyLogin = structuredClone(postLogin);
+    bodyLogin.senha = "123456";
+    const response = await request(app)
+      .post("/api/auth/login")
+      .set("Content-Type", "application/json")
+      .send(bodyLogin);
+
+    expect(response.status).to.equal(401);
+    expect(response.body.error).to.equal("E-mail ou senha inválidos.");
+  });
+  it("login deve retornar 401 quando o e-mail informado for incorreto", async () => {
+    const bodyLogin = structuredClone(postLogin);
+    bodyLogin.email = "teste@teste.com";
+    const response = await request(app)
+      .post("/api/auth/login")
+      .set("Content-Type", "application/json")
+      .send(bodyLogin);
+
+    expect(response.status).to.equal(401);
+    expect(response.body.error).to.equal("E-mail ou senha inválidos.");
   });
 });
