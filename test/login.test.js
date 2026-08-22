d09482635f65fcdb2e3362b0adee0b5a7ea14225
Gustavo Franco Morales <gustavo050899.morales@gmail.com>
2026-08-22 09:39:23 -0300

    Ajusta script de teste de api de login

--- test/login.test.js
+++ test/login.test.js
@@ -1,16 +1,16 @@
 import request from "supertest";
 import app from "../src/app.js";
 import { expect } from "chai";
+import postLogin from "../fixtures/postLogin.json" with { type: "json" };
+
 
 describe("Login", () => {
   it("login deve retornar 200 quando o usuario e senha forem corretos", async () => {
+    const bodyLogin = structuredClone(postLogin)
     const response = await request(app)
       .post("/api/auth/login")
       .set("Content-Type", "application/json")
-      .send({
-        email: "admin@escola.com",
-        senha: "admin123",
-      });
+      .send(bodyLogin);
     expect(response.status).to.equal(200);
     expect(response.body.token).to.be.a('string');
     expect(response.body.usuario.id).to.equal('admin-principal');
